dbebbca47b728490ab84c023b60fe323d6d17be1
Afif Zafri <zafri_21596@yahoo.com>
2016-06-08 10:44:03 +0800

    Update view.php

--- view.php
+++ view.php
@@ -4,13 +4,24 @@ include './templates/header.php';
 ?>
 
 <div class="noprint">
-<h1 class='page-header'>View Logs</h1>
-<form action='view.php' method="post" id="viewLog">
-<b>Choose Date : </b><input type="date" name="date">
+	<h1 class='page-header'>View Logs</h1>
+	<div class="col-lg-12">
+		<form action='view.php' method="post" id="viewLog">
+			<table>
+				<tr>
+					<th>Choose Date : </th><td><div class="col-xs-15"><input type="date" name="date" class="form-control" id="selectDate"></div></td>
+				</tr>
+			</table>
+		</form>
+	</div>
 </div>
+
 <br>
+
+<div class="col-lg-12">
 <div class="box-body table-responsive " id="responsivetablediv">
 	<div id="showLog"></div>
+</div>
 </div>
 
 
