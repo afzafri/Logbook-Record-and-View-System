588fc0c174586354dc9869177437d8fe60de3314
Afif Zafri <zafri_21596@yahoo.com>
2019-02-19 09:20:59 +0800

    convert the date format

--- process.php
+++ process.php
@@ -16,36 +16,38 @@ catch(PDOException $e)
 //add log
 if((isset($_POST['option'])) == "add")
 {
-		
+
   $act = (isset($_POST['act']) ? $_POST['act'] : null);
   $date = (isset($_POST['date']) ? $_POST['date'] : null);
   $time = (isset($_POST['time']) ? $_POST['time'] : null);
-  
+
   if(isset($_POST['act']) && isset($_POST['date']) && isset($_POST['time']))
   {
-	try
-	{
+		$date = DateTime::createFromFormat('d/m/Y', $_POST['date']);
 		
-		$stmt = $conn->prepare("INSERT INTO LOGBOOK (ACT,DATE,TIME) VALUES(?,?,?) ");
-		$stmt->execute(array($act,$date,$time));
-		
-	}
-	catch(PDOException $e)
-	{
-		echo "Connection Error : " . $e->getMessage();
-	}  
+		try
+		{
+
+			$stmt = $conn->prepare("INSERT INTO LOGBOOK (ACT,DATE,TIME) VALUES(?,?,?) ");
+			$stmt->execute(array($act,$date->format('Y-m-d'),$time));
+
+		}
+		catch(PDOException $e)
+		{
+			echo "Connection Error : " . $e->getMessage();
+		}
   }
-	
+
 }
 
 //view log
 if((isset($_POST['option'])) == "view")
 {
-  $date = $_POST['date'];
-  
+	$date = DateTime::createFromFormat('d/m/Y', $_POST['date']);
+
   echo "
   <div class='noprint'>
-  <h3>Practical Training Log Book Entry - ".date('d/m/Y',strtotime($date))."</h3></div>
+  <h3>Practical Training Log Book Entry - ".$_POST['date']."</h3></div>
   <div id='logtable'>
   <table width='100%' class='table table-bordered table-hover'>
   <tr>
@@ -54,45 +56,45 @@ if((isset($_POST['option'])) == "view")
   <th>Supervisor Remark
   </th>
   </tr>
-  
+
   ";
   try
 	{
-		
+
 		$stmt = $conn->prepare("SELECT ACT,DATE,DATE_FORMAT(TIME,'%h:%i %p') AS NEWTIME FROM LOGBOOK WHERE DATE=?");
-		$stmt->execute(array($date));
-		
+		$stmt->execute(array($date->format('Y-m-d')));
+
 		while($result=$stmt->fetch(PDO::FETCH_ASSOC))
 		{
 			$dates = $result['DATE'];
 			$act = $result['ACT'];
 			$time = $result['NEWTIME'];
-			
+
 			echo "
-			
+
 			<tr>
 			<td>".date('d/m/Y',strtotime($dates))." <br>- $time</td>
 			<td><li>$act</td>
 			<td></td>
 			</tr>
-			
+
 			";
-			
+
 		}
-		
+
 	}
 	catch(PDOException $e)
 	{
 		echo "Connection Error : " . $e->getMessage();
 	}
-	
+
 	echo "</table>
 	</div>
 	<br><br>
 	";
-	
+
 	//generate field for Supervisor signature every Friday
-	if((date('D',strtotime($date))) == "Fri")
+	if((date('D',strtotime($_POST['date']))) == "Fri")
 	{
 		echo "
 		<div id='logtable'>
@@ -104,15 +106,15 @@ if((isset($_POST['option'])) == "view")
 		</div>
 		";
 	}
-	
+
 	echo"
 	<br><br><br>
 	<div class='noprint'>
 	<input type='button' class='btn btn-primary' value='Print' id='printBut'/>
 	</div>
-	
+
 	";
-	
+
 }
 
 
