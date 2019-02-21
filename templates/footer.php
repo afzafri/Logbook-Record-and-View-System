4702c35a4fc69bc3e7ed09626f3551ba8a5ab98f
Afif Zafri <zafri_21596@yahoo.com>
2019-02-21 15:01:27 +0800

    load process js after all the plugins are loaded

--- templates/footer.php
+++ templates/footer.php
@@ -10,11 +10,8 @@
 
     <!-- /#wrapper --></div>
 
-	<!-- JQuery -->
+	  <!-- JQuery -->
     <script src="./templates/js/jquery.js"></script>
-
-	<!-- Core Script -->
-    <script src="./templates/js/script.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="./templates/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -36,6 +33,9 @@
 
     <!-- Custom Theme JavaScript -->
     <script src="./templates/dist/js/sb-admin-2.js"></script>
+
+    <!-- Core Script -->
+    <script src="./templates/js/script.js"></script>
 
 </body>
 
